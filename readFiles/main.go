17c46c6b81d254c0553a88f026426edbef6efbda
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2022-12-06 17:58:13 +0530

    Added new commits

--- readFiles/main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-/*
-	Goal: process a file a line at a time but instead of doing it in serial, you can use goroutines and channels
-*/
--- resolveDomain/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-/*
-	Goal:- Resolving domain names using Golang Channels
-		1. A function that resolves the domain name.
-		2. instead of going from the list of domains to the function that resolves them to the output feed the domains into a channel
-		2. start multiple processing goroutines. They can each pull from the channel.
-*/
