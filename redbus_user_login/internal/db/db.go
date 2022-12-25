046567220cd41d9af9eea515b0c6c03173db0324
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2022-12-25 10:04:27 +0530

    COL-1 Apply Singleton connections for DB

--- redbus_user_login/internal/db/db.go
+++ redbus_user_login/internal/db/db.go
@@ -12,23 +12,35 @@ type Database struct {
 	Client *sqlx.DB
 }
 
+// Singleton object - where
+// File descriptor, total limit on
+// New ports will also open
+var (
+	dbClient *Database
+)
+
 func NewDatabase() (*Database, error) {
-	connectionString := fmt.Sprintf(
-		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USERNAME"),
-		os.Getenv("DB_TABLE"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("SSL_MODE"),
-	)
-	dbConn, err := sqlx.Connect("postgres", connectionString)
-	if err != nil {
-		return &Database{}, fmt.Errorf("Could not connect to the Database:%w", err)
+	fmt.Println(dbClient)
+	if dbClient == nil {
+		connectionString := fmt.Sprintf(
+			"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USERNAME"),
+			os.Getenv("DB_TABLE"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("SSL_MODE"),
+		)
+		conn, err := sqlx.Connect("postgres", connectionString)
+		if err != nil {
+			return &Database{}, fmt.Errorf("Could not connect to the Database:%w", err)
+		}
+		// Whenever we are accessing a pointer ,  check null pointer exception
+		dbClient = &Database{
+			Client: conn,
+		}
 	}
-	return &Database{
-		Client: dbConn,
-	}, nil
+	return dbClient, nil
 }
 
 func (d *Database) Ping(ctx context.Context) error {
