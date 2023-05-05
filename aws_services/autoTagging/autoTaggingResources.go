23afb1a9c6c2262d2cb100ebad64ae642993cd5b
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2023-05-05 12:03:39 +0530

    tagging capture for instances

--- aws_services/autoTagging/autoTaggingResources.go
+++ aws_services/autoTagging/autoTaggingResources.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
-var ec2client *ec2.Client
+type Instance struct {
+	InstanceID string    `json:"instance_id"`
+	Tags       []TagType `json:"Tags"`
+}
+type TagType struct {
+	Key   string `json:"Key"`
+	Value string `json:"Value"`
+}
+
+type Instances struct {
+	Instances []*Instance `json:"instances"`
+}
+
+var (
+	ec2client *ec2.Client
+)
 
 func init() {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
@@ -22,6 +38,7 @@ func init() {
 
 // GettingTag - The Function Queries all the ec2 instances and ebs volumes about Tags
 func GettingTag() {
+	var ins []*Instance
 	params := &ec2.DescribeInstancesInput{
 		MaxResults: aws.Int32(10),
 	}
@@ -30,19 +47,25 @@ func GettingTag() {
 		fmt.Println("Error Calling ec2:", err)
 		return
 	}
-	count := len(result.Reservations)
-	fmt.Println("Instances:", count)
 
-	for i, reservation := range result.Reservations {
-		for k, instance := range reservation.Instances {
-			fmt.Println("Instance number: ", i, "-", k, "Id: ", *instance.InstanceId)
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			tags := []TagType{}
 			for _, tag := range instance.Tags {
-				fmt.Println("Instance Tags key:", *tag.Key)
-				fmt.Println("Instance Tags Value:", *tag.Value)
+				tagstype := TagType{Key: *tag.Key, Value: *tag.Value}
+				tags = append(tags, tagstype)
 			}
+			ec2Tag := &Instance{
+				InstanceID: *instance.InstanceId,
+				Tags:       tags}
+			ins = append(ins, ec2Tag)
 		}
 	}
+	p, _ := json.MarshalIndent(Instances{Instances: ins}, " ", "\t")
+	fmt.Printf("%s\n", p)
 }
+
+//TODO - Need to store the tags and later compare if needed more tags
 
 // TagNotProper - This function checks whether tags which are not proper matching SSM
 func TagNotProper() {
